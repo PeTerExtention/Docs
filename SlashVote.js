2d31ac41bca766af749f1fb5a162c296d80a3d86
PeTerYT <76822417+PeTer344-dot@users.noreply.github.com>
2023-02-16 13:11:03 +0100

    Update SlashVote.js

--- SlashVote.js
+++ SlashVote.js
@@ -1,17 +1,24 @@
-const { CommandInteraction } = require("discord.js");
+const {
+  CommandInteraction,
+  ApplicationCommandType,
+  PermissionFlagsBits,
+  ActionRowBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+  ButtonBuilder,
+} = require("discord.js");
 const JUGNU = require("../../../handlers/Client");
 const { Queue } = require("distube");
 const { links } = require("../../../settings/config");
 
 module.exports = {
   name: "vote",
-  aliases: ["vo", "v"],
-  description: `Help me to Grow up By Voting`,
-  userPermissions: ["SEND_MESSAGES"],
-  botPermissions: ["EMBED_LINKS"],
+  description: `Vote For Musico!`,
+  userPermissions: PermissionFlagsBits.SendMessages,
+  botPermissions: PermissionFlagsBits.EmbedLinks,
   category: "Information",
   cooldown: 5,
-  type: "CHAT_INPUT",
+  type: ApplicationCommandType.ChatInput,
   inVoiceChannel: false,
   inSameVoiceChannel: false,
   Player: false,
@@ -26,9 +33,21 @@ module.exports = {
    */
   run: async (client, interaction, args, queue) => {
     // Code
-    client.embed(
-    interaction,
-      `<:Discord_Inbox:1071901344069337118>[\`Vote Me On Top.gg\`](https://top.gg/bot/810540985032900648?s=0ccd00208d573)`
+    const row = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+        .setURL('https://www.top.gg')
+        //.setEmoji('<:topgg:1075574316151881800>')
+        .setLabel('Vote Me On Top.gg')
+        .setStyle(ButtonStyle.Link),
     );
+
+      const embed = new EmbedBuilder()
+			.setColor(client.config.embed.color)
+			.setTitle('Vote For Musico !')
+			.setURL('https://discord.gg/9cAXP5j5Rc')
+			.setDescription('Click on the Button to Vote for Musico !');
+
+      await interaction.editReply({ ephemeral: true, embeds: [embed], components: [row] });
   },
 };
